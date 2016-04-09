dd4cb31979e948f378193299cc4593ae68ad2b5e
Michael Wheatley <michaelvwheatley@gmail.com>
2016-04-09 12:15:28 -0500

    text

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,7 @@ var Sequelize = require('sequelize');
 var env       = process.env.NODE_ENV || "development";
 var config    = require(Path.join(__dirname, '../..', 'config', 'config.json'))[env];
 // var sequelize = new Sequelize(config.database, config.username, config.password);
-var sequelize = new Sequelize(CLEARDB_DATABASE_URL);
+var sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 
 
 db.Question = sequelize.import('./question.js');
